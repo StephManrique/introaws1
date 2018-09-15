988590cdea95263f3b7d88410511ec819bc3801c
EC2 Default User <ec2-user@ip-172-31-34-157.us-east-2.compute.internal>
2018-09-15 15:54:14 +0000

    nuevo

--- clase7/backend/app/routes.js
+++ clase7/backend/app/routes.js
@@ -42,11 +42,11 @@ module.exports = function (app) {
     });
 
     // delete a todo
-    app.delete('/api/todos/:todo_id', function (req, res) {
+    app.delete('/api/todos', function (req, res) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         Todo.remove({
-            _id: req.params.todo_id
+            _id: req.query.id
         }, function (err, todo) {
             if (err)
                 res.send(err);
